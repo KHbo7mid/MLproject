d231f27f522319b80361aa7e20d27f8d054337b1
ahmed <khiari.ahmed2002@gmail.com>
2025-10-15 11:27:17 +0100

    modified setup

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
 from typing import List
+
 HYPHEN_E_DASH = '-e .'
 def get_requirements(file_path:str)->List[str]:
     ''''This function will return the list of requirements'''
